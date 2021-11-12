94069cad2f226d84186041c58064178ef0ff2bd4
THBSimon <simon.wichura@th-brandenburg.de>
2021-11-12 16:57:39 +0100

    Klassen für Pflanze und Sensor erstellt/generiert.

---
+++ src/main/java/com/example/BackendPlanti/Pflanze.java
@@ -0,0 +1,62 @@
+package com.example.BackendPlanti;
+
+public class Pflanze {
+
+    private int PID;
+    private String Name;
+    private int Standort;
+    private int Schwierigkeit;
+    private String Merkmale;
+    private String WissName;
+
+
+    public Pflanze() {}
+
+    public int getPID() {
+        return PID;
+    }
+
+    public void setPID(int PID) {
+        this.PID = PID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getStandort() {
+        return Standort;
+    }
+
+    public void setStandort(int standort) {
+        Standort = standort;
+    }
+
+    public int getSchwierigkeit() {
+        return Schwierigkeit;
+    }
+
+    public void setSchwierigkeit(int schwierigkeit) {
+        Schwierigkeit = schwierigkeit;
+    }
+
+    public String getMerkmale() {
+        return Merkmale;
+    }
+
+    public void setMerkmale(String merkmale) {
+        Merkmale = merkmale;
+    }
+
+    public String getWissName() {
+        return WissName;
+    }
+
+    public void setWissName(String wissName) {
+        WissName = wissName;
+    }
+}
---
+++ src/main/java/com/example/BackendPlanti/Sensor.java
@@ -0,0 +1,26 @@
+package com.example.BackendPlanti;
+
+public class Sensor {
+
+    private int SID;
+    private String Link;
+
+    public Sensor() {
+    }
+
+    public int getSID() {
+        return SID;
+    }
+
+    public void setSID(int SID) {
+        this.SID = SID;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+}
